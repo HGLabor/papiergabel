9d95ce4bea650cc3dcba4fcc2a443dde2c69a39d
BuildTools <unconfigured@null.spigotmc.org>
2022-06-12 09:55:32 +0200

    Paper 1.19 and rebranding

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,6 @@ pluginManagement {
     }
 }
 
-rootProject.name = "forktest"
+rootProject.name = "papiergabel"
 
-include("forktest-api", "forktest-server")
+include("papiergabel-api", "papiergabel-server")
